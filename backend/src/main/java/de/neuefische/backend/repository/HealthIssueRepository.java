40c9839b04062a6e9c65ade6b6fd4acd91fdbc86
sherzad <t.sherzad@web.de>
2024-02-11 23:13:34 +0100

    Create-HealthIssue-Model-Repo-Serivce-Controller

---
+++ backend/src/main/java/de/neuefische/backend/repository/HealthIssueRepository.java
@@ -0,0 +1,6 @@
+package de.neuefische.backend.repository;
+import de.neuefische.backend.model.HealthIssue;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface HealthIssueRepository extends MongoRepository<HealthIssue, String> {
+}
---
+++ backend/src/main/java/de/neuefische/backend/service/HealthIssueService.java
@@ -0,0 +1,31 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.model.HealthIssue;
+import de.neuefische.backend.repository.HealthIssueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HealthIssueService {
+
+    private final HealthIssueRepository healthIssueRepository; // Declare the field
+
+    @Autowired
+    public HealthIssueService(HealthIssueRepository healthIssueRepository) {
+        this.healthIssueRepository = healthIssueRepository;
+    }
+
+    public List<HealthIssue> getAllHealthIssues() {
+        return healthIssueRepository.findAll();
+    }
+
+    public HealthIssue addHealthIssue(HealthIssue issue) {
+        return healthIssueRepository.save(issue);
+    }
+
+    public void removeHealthIssue(String issueId) {
+        healthIssueRepository.deleteById(issueId);
+    }
+}
